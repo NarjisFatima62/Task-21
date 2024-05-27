34ef1618e8e67357cce89e35f0f174364221665b
Narjis Fatima <161502591+NarjisFatima62@users.noreply.github.com>
2024-05-27 22:07:13 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+"use strict";
+//Question No.21:
+//They think of something you could store in a TypeScript Object. Write a program that creates Objects containing these items.
+//Answer:
+let person = { name: " Narjis", fname: "female", age: 23 };
+console.log(person);
---
+++ main.ts
@@ -0,0 +1,6 @@
+//Question No.21:
+//They think of something you could store in a TypeScript Object. Write a program that creates Objects containing these items.
+
+//Answer:
+let person: {name: string, fname: string, age: Number} = {name: " Narjis", fname: "female", age: 23}
+console.log(person);
